1897379e9ac12dc48c2424127d01ec164ffcb4cd
JRDR <u_062pn>
2022-06-04 20:19:04 +0300

    Added id to Product model

--- src/main/java/com/IFCDPP/IFCDPP/service/ProductService.java
+++ src/main/java/com/IFCDPP/IFCDPP/service/ProductService.java
@@ -21,7 +21,7 @@ public class ProductService {
     }
 
     private Product mapEntityForCatalog(ProductEntity entity) {
-        return Product.builder().title(entity.getTitle()).description(entity.getDescription())
+        return Product.builder().id(entity.getId()).title(entity.getTitle()).description(entity.getDescription())
                 .developer(entity.getDeveloper()).price(entity.getPrice()).downloadLink(entity.getDownloadLink())
                 .category(entity.getCategory() == null ? "Без категории" : entity.getCategory().getTitle())
                 .imageLink(entity.getImageLink())
