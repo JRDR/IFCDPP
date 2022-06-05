0365e084412d3046714325dfdb590ef0989e3df0
JRDR <u_062pn>
2022-06-05 15:54:02 +0300

    Removed unnecessary logging

--- src/main/resources/static/js/catalog.js
+++ src/main/resources/static/js/catalog.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
     $(document).on('click', '.page-button', function (e) {
         let categoryId = $(".current-category").val();
         let page = $(this).val();
-        console.log(categoryId + " " + page);
         location.href = "/catalog?categoryId=" + categoryId + "&page=" + page;
     });
 
