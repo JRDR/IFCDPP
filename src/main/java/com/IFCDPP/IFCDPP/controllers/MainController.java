b1d9d8164e279882da01f63c5f4d8e95a931d8ee
JRDR <a1732b@yandex.ru>
2022-06-07 08:10:28 +0300

    new pages

--- src/main/java/com/IFCDPP/IFCDPP/controllers/MainController.java
+++ src/main/java/com/IFCDPP/IFCDPP/controllers/MainController.java
@@ -3,6 +3,7 @@ package com.ifcdpp.ifcdpp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MainController {
@@ -18,5 +19,22 @@ public class MainController {
         return "about";
     }
 
+    @GetMapping("/support")
+    public String support(Model model) {
+        model.addAttribute("title", "Поддержка");
+        return "support";
+    }
+
+    @GetMapping("/contacts")
+    public String contacts(Model model) {
+        model.addAttribute("title", "Поддержка");
+        return "contacts";
+    }
+
+    @GetMapping("/devinfo")
+    public String devinfo(Model model) {
+        model.addAttribute("title", "Поддержка");
+        return "devinfo";
+    }
 
 }
